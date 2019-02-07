4f933ba5fe450fc5d06e793d2b1658e5d310ce76
Christopher Holmes <chrisholmes@chriss-macbook-pro.local>
2019-02-07 13:49:21 -0500

    Initial Commit

---
+++ HW1DataStruct/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW1DataStruct
+//
+//  Created by Chris holmes on 2/7/19.
+//  Copyright © 2019 Chris holmes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
